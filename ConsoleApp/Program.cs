2bb12fc5cc353f7307189dfc8c33c65515324752
kurtzpb <kurtzpb@gmail.com>
2016-01-21 22:44:31 +0200

    New task

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,9 +8,49 @@ namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello!");
+            double operand1 = 56;
+            double operand2 = 0;
+
+            Console.WriteLine("Введите оператор:");
+            string sign = Console.ReadLine();
+
+            switch (sign)
+            {
+                case "+":
+                    {
+                        Console.Write(operand1 + operand2);
+                        break;
+                    }
+                case "-":
+                    {
+                        Console.Write(operand1 - operand2);
+                        break;
+                    }
+                case "*":
+                    {
+                        Console.Write(operand1 * operand2);
+                        break;
+                    }
+                case "/":
+                    {
+                        if (operand2 != 0)
+                        {
+                            Console.Write(operand1 / operand2);
+                        }
+                        else { Console.WriteLine("Делитель не должен быть равен нулю"); }
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Вы ввели не корректный оператор!");
+                        break;
+                    }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
         }
     }
 }
